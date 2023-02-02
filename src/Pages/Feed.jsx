197d4d178451715e52a5d26e64e2c0fafdb0c8b3
kanadeakshay <akshayk0782@gmail.com>
2023-02-03 02:13:07 +0530

    :sparkles: add: feed empty page

---
+++ src/Pages/Feed.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Sidebar from '../Components/Sidebar/Sidebar'
+
+const Feed = () => {
+  return (
+    <>
+      <Sidebar />
+      <div>Feed</div>
+    </>
+  )
+}
+
+export default Feed
