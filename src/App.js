f3a4edbb1031eae27cbd955ba9a749893d9e1e13
Pranav Dani <pranavpdani@gmail.com>
2023-02-02 22:19:39 +0530

    🛠️ fix: refactor file locations

--- src/App.js
+++ src/App.js
@@ -1,55 +1,55 @@
-import "./App.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Login from "./Components/Login";
-import LoginPhone from "./Components/LoginPhone";
-import Profile from "./Components/Profile";
-import { getAuth } from "firebase/auth";
-import { useAuthState } from "react-firebase-hooks/auth";
-import { useEffect } from "react";
-import Navbar from "./Components/Navbar";
-import RentForm from "./Components/RentForm";
-import Landing from "./Pages/Landing";
+import './App.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Login from './Components/Login/Login'
+import LoginPhone from './Components/LoginPhone'
+import Profile from './Components/Profile'
+import { getAuth } from 'firebase/auth'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { useEffect } from 'react'
+import Navbar from './Components/Navbar/Navbar'
+import RentForm from './Components/RentForm'
+import Landing from './Pages/Landing'
 
-function App() {
-  const [user, loading, error] = useAuthState(getAuth());
+function App () {
+  const [user, loading, error] = useAuthState(getAuth())
   useEffect(() => {
     if (loading) {
-      console.log("Loading");
+      console.log('Loading')
     } else if (error) {
-      console.log(error);
+      console.log(error)
     } else if (user) {
-      console.log(user);
+      console.log(user)
     }
-  }, [loading, error, user]);
+  }, [loading, error, user])
   const router = createBrowserRouter([
     {
-      path: "/",
-      element: <Landing />,
+      path: '/',
+      element: <Landing />
     },
     {
-      path: "/login",
-      element: <Login />,
+      path: '/login',
+      element: <Login />
     },
     {
-      path: "/login/phone",
-      element: <LoginPhone />,
+      path: '/login/phone',
+      element: <LoginPhone />
     },
     {
-      path: "/profile",
-      element: user ? <Profile user={user} /> : <Login />,
+      path: '/profile',
+      element: user ? <Profile user={user} /> : <Login />
     },
     {
-      path: "/rent",
-      element: <RentForm />,
-    },
-  ]);
+      path: '/rent',
+      element: <RentForm />
+    }
+  ])
 
   return (
     <>
       <Navbar user={user} />
       <RouterProvider router={router} />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/Components/Login/Login.js
@@ -0,0 +1,45 @@
+import { signInWithEmailAndPassword, getAuth } from 'firebase/auth'
+import { useState } from 'react'
+import Google from '../Google'
+
+export default function Form (props) {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  return (
+    <>
+      <div>
+        <form
+          onSubmit={e => {
+            e.preventDefault()
+            signInWithEmailAndPassword(getAuth(), email, password)
+              .then(userCredential => {
+                const user = userCredential.user
+                console.log(user)
+              })
+              .catch(error => {
+                // const errorCode = error.code;
+                const errorMessage = error.message
+                alert(errorMessage)
+              })
+          }}
+        >
+          <h1>{props.title}</h1>
+          <input
+            type='email'
+            placeholder='Email'
+            value={props.email}
+            onChange={e => setEmail(e.target.value)}
+          />
+          <input
+            type='password'
+            value={props.password}
+            placeholder='Password'
+            onChange={e => setPassword(e.target.value)}
+          />
+          <button type='submit'>Submit</button>
+        </form>
+        <Google title='Sign In with Google' />
+      </div>
+    </>
+  )
+}
